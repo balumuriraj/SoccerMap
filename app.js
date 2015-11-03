842f6c34495b2f6f54275d8cb1fd66a5f0098d17
MohanRaj Balumuri <MBalumuri@esri.com>
2015-11-03 10:28:20 -0800

    added layer

--- app.js
+++ app.js
@@ -6,16 +6,30 @@ require(
 
     "esri/geometry/Extent",
     "esri/geometry/SpatialReference",
-
+    
+    "esri/layers/ArcGISTiledLayer",
+    "esri/layers/FeatureLayer",
+     
+    "esri/renderers/UniqueValueRenderer", 
+    
+    "esri/symbols/SimpleLineSymbol", 
+    "esri/symbols/SimpleFillSymbol",
+    
+    "esri/Color",
+     
     "dojo/domReady!"
   ], function (
     Map, MapView, SceneView,
-    Extent, SpatialReference
+    Extent, SpatialReference,
+    ArcGISTiledLayer, FeatureLayer,
+    UniqueValueRenderer,
+    SimpleLineSymbol, SimpleFillSymbol,
+    Color
   ) {
 
     /**** Maps ****/
     var map1 = new Map({
-      basemap: "satellite"
+      //basemap: "satellite"
     });
 
     var map2 = new Map({
@@ -34,9 +48,23 @@ require(
     });
 
     /**** 2D ****/
-    view2D.on("click", function(){
-      console.log(view2D.extent);
+    // view2D.on("click", function(){
+    //   console.log(view2D.extent);
+    // });
+    
+    /**** Layer ****/
+    var baselyr = new ArcGISTiledLayer({
+      url: "http://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer"
     });
+    
+    map1.add(baselyr);
+    
+    var lyr = new FeatureLayer({
+      url: "http://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/World_Countries_(Generalized)/FeatureServer/0",
+      opacity: 0.95
+    });
+    
+    map1.add(lyr);
 
     var init2DExtent = new Extent({
       xmax: 19407422.51677575,
@@ -49,10 +77,33 @@ require(
     });
 
     view2D.then(function() {
-      view2D.animateTo(init2DExtent);
+      
+      lyr.then(function() {
+        var defaultSymbol = new SimpleFillSymbol({
+          color: "#2ecc71"
+        });
+  
+        var renderer = new UniqueValueRenderer(defaultSymbol, "Country");
+        
+        console.log(lyr);
+    
+        //add symbol for each possible value
+        //renderer.addValue("Pacific", new SimpleFillSymbol().setColor(new Color([255, 0, 0, 0.5])));
+        
+        lyr.renderer = renderer;
+        
+        view2D.animateTo(init2DExtent);
+        
+      });
     });
 
     /**** 3D ****/
+    
+    
+    
+    
+    
+    
 
   }
 );
